dd1f162490ab17f4aa08a98abd4615fae91cfdf5
Roman <blackwanted@yandex.ru>
2021-02-12 16:21:37 +0300

    -- Update

--- src/client/GL_Client.ts
+++ src/client/GL_Client.ts
@@ -121,6 +121,7 @@ export class GL_Client {
                 code: e.keyCode,
             });
         });
+
         document.addEventListener("wheel", (e: WheelEvent) => {
             this.send({
                 type: "zoom",
